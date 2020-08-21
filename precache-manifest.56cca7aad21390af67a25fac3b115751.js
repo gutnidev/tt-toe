a9f5426aae21749ed5068e63ae448075b50439de
gutnivlad <gutnivlad@gmail.com>
2020-08-21 15:24:11 +0300

    deploy

---
+++ precache-manifest.56cca7aad21390af67a25fac3b115751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7708c63a4defcfc5118a",
+    "url": "/tt-toe/css/app.781a127e.css"
+  },
+  {
+    "revision": "5fb20147cebac253a4ce94518936fbba",
+    "url": "/tt-toe/index.html"
+  },
+  {
+    "revision": "7708c63a4defcfc5118a",
+    "url": "/tt-toe/js/app.acf64af2.js"
+  },
+  {
+    "revision": "4e6d363ee817e5a5a170",
+    "url": "/tt-toe/js/chunk-vendors.8c2177dc.js"
+  },
+  {
+    "revision": "8582053db2c010b64342d101b1d5fa94",
+    "url": "/tt-toe/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tt-toe/robots.txt"
+  }
+]);
